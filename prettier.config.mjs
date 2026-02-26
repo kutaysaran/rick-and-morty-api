626e21fd8789ad117391e12fc580bd020e5d5d95
Kutay Saran <74209499+kutaysaran@users.noreply.github.com>
2026-02-26 03:11:25 +0300

    .

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -3,9 +3,11 @@ const config = {
   semi: true,
   singleQuote: false,
   trailingComma: "all",
+  arrowParens: "always",
   printWidth: 100,
   tabWidth: 2,
   plugins: ["prettier-plugin-tailwindcss"],
+  tailwindFunctions: ["cn"],
 };
 
 export default config;
